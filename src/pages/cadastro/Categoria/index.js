4669771acd738837cb17700c2c41a598f38f688d
davidlpc1 <davipadrao76@gmai.com>
2020-07-31 23:56:06 -0300

    Atualização parte 1 da aula 4

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,98 +1,111 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
 import FormField from '../../../components/FormField';
-
-
+import Button from '../../../components/Button';
 
 function CadastroCategoria() {
-
-  const [categorias, setCategorias] = useState([]);
-
   const valoresIniciais = {
     nome: '',
-    descrição: '',
+    descricao: '',
     cor: '',
-  }
-
+  };
+  const [categorias, setCategorias] = useState([]);
   const [values, setValues] = useState(valoresIniciais);
 
-  function setValue(chave, valor){
+  function setValue(chave, valor) {
+    // chave: nome, descricao, bla, bli
     setValues({
       ...values,
-      [chave] : valor,
-    })
+      [chave]: valor, // nome: 'valor'
+    });
   }
 
   function handleChange(infosDoEvento) {
     setValue(
       infosDoEvento.target.getAttribute('name'),
-      infosDoEvento.target.value
+      infosDoEvento.target.value,
     );
   }
 
+  // ============
+
+  useEffect(() => {
+    if (window.location.href.includes('localhost')) {
+      const URL = 'http://localhost:8080/categorias';
+      fetch(URL)
+        .then(async (respostaDoServer) => {
+          if (respostaDoServer.ok) {
+            const resposta = await respostaDoServer.json();
+            setCategorias(resposta);
+            return;
+          }
+          throw new Error('Não foi possível pegar os dados');
+        });
+    }
+  }, []);
+
   return (
     <PageDefault>
-      <h1>Cadastro de Categoria: {values.nome}</h1>
+      <h1>
+        Cadastro de Categoria:
+        {values.nome}
+      </h1>
 
-      <form onSubmit={function handleSubmit(infosDoEvento){
+      <form onSubmit={function handleSubmit(infosDoEvento) {
         infosDoEvento.preventDefault();
-        console.log('Voce tentou enviar o form sksksksksks');
+
         setCategorias([
           ...categorias,
-          values
+          values,
         ]);
-        
+
         setValues(valoresIniciais);
-      }}>
+      }}
+      >
 
-      <FormField
-        label="Nome da Categoria:"
-        values={values.nome}
-        onChange={handleChange}
-        type="text"
-        name="nome"
-      />
-        <div>
-          <label>
-            Descrição:
-            <textarea
-              type="text"
-              value={values.descrição}
-              name="descrição"
-              onChange={handleChange}
-              />
-            </label>
-        </div>
         <FormField
-        label="Cor:"
-        values={values.cor}
-        onChange={handleChange}
-        type="color"
-        name="cor"
-      />
+          label="Nome da Categoria"
+          type="text"
+          name="nome"
+          value={values.nome}
+          onChange={handleChange}
+        />
 
-        <button>
+        <FormField
+          label="Descrição:"
+          type="textarea"
+          name="descricao"
+          value={values.descricao}
+          onChange={handleChange}
+        />
+
+        <FormField
+          label="Cor"
+          type="color"
+          name="cor"
+          value={values.cor}
+          onChange={handleChange}
+        />
+
+        <Button>
           Cadastrar
-        </button>
+        </Button>
       </form>
-      
 
       <ul>
-          {categorias.map((categoria,indice) => {
-            return(
-              <li key={`${categoria}${indice}`}>
-                {categoria.nome}
-              </li>
-            )
-          })}
+        {categorias.map((categoria) => (
+          <li key={`${categoria.nome}`}>
+            {categoria.titulo}
+          </li>
+        ))}
       </ul>
 
       <Link to="/">
         Ir para home
       </Link>
     </PageDefault>
-  )
+  );
 }
 
 export default CadastroCategoria;
